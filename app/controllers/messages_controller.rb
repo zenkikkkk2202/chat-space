e72739e4e4e3020caf6677ba2fdc28cbfd9ef57e
zenkikkkk2202 <57906987+zenkikkkk2202@users.noreply.github.com>
2019-11-27 11:43:47 +0900

    画像投稿の修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,8 +7,10 @@ class MessagesController < ApplicationController
   end
 
   def create
+    
     @message = @group.messages.new(message_params)
     if @message.save
+      
       redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
     else
       @messages = @group.messages.includes(:user)
